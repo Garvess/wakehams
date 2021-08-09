4059101ab01116f9ac35bc5ef7c5793e7f041dad
Garvess <gary_john50@hotmail.com>
2021-08-08 23:51:45 -0230

    Got insert.cpp working much better.

--- insert.cpp
+++ insert.cpp
@@ -39,7 +39,20 @@ class db_response
 
 };
 
-void RegisterStudent();
+void InsertFirstName();
+//void InsertLastName();
+
+//bool thingHappened = false;
+
+//    void Update(){
+
+//        if(!thingHappened && InsertFirstName){
+//            thingHappened = true;
+//            InsertFirstName();
+//        }
+
+
+//}
 
 int main(){
 
@@ -47,46 +60,110 @@ int main(){
 
     //Call Methods
     db_response::ConnectionFunction();
-    RegisterStudent();
+
+
+  //  Update();
+
+    InsertFirstName();
+    //InsertLastName();
+
+
 
     return 0;
 }
 
-void RegisterStudent()
+void InsertFirstName()
     {
         //Initial load
         system("cls");
 
-        //Variables
-        string client_id = "";
         string first_name = "";
-
+        string town = "";
 
         cout << "School management system." << endl << endl;
 
+        cout << "Kill program with ctrl c only." << endl << endl;
         cout << "Enter Client First Name:";
 
         getline(cin, first_name);
+
+        cout << "Push that shit";
+        getline(cin, town);
         //cout << "Enter Client ID Number:";
         //cin >> client_id)
 
-        string insert_query1 = "INSERT INTO clients (first_name) VALUES ('"+ first_name +"')";
+        string insert_query1 = "INSERT INTO clients (first_name,town) VALUES ('"+ first_name + "','"+ town +"')";
 
         const char* q = insert_query1.c_str();
 
-//Idea: Create another "insert_query string". Declare another char pointer "insert_query2 etc".
-
         qstate = mysql_query(conn,q);
 
-        if(!qstate)
-            {
-                cout << endl << "Registration success" << endl;
-                system("pause");
-                main();
-            }
-        else
-            {
-                cout << "Query Execution Problem!" << mysql_errno(conn) << endl;
-            }
+// My shit under here.
 
-}
+        //system("cls");
+        //string town = "";
+
+        //cout << "Enter Client Town:" << endl << endl;
+        //getline(cin, town);
+        //insert_query1 = "INSERT INTO clients (town) VALUES ('"+ town +"')";
+        //const char* u = insert_query1.c_str();
+
+        //qstate = mysql_query(conn,u);
+
+    }
+
+
+
+//        if(!qstate)
+  //          {
+    //            cout << endl << "Registration success" << endl;
+      //          system("pause");
+      //          main();
+       //     }
+        //else
+         //   {
+          //      cout << "Query Execution Problem!" << mysql_errno(conn) << endl;
+           // }
+
+//}
+
+//void InsertLastName()
+//    {
+//        //Initial load
+//        system("cls");
+
+        //Variables
+//        string last_name = "";
+//       string town = "";
+
+//        cout << "School management system." << endl << endl;
+
+//        cout << "Enter Client Last Name:";
+
+//        getline(cin, last_name);
+        //cout << "Enter Client ID Number:";
+        //cin >> client_id)
+
+       //cout << "Enter Client Town:";
+
+
+//        string insert_query1 = "INSERT INTO clients (last_name) VALUES ('"+ last_name +"')";
+
+//        const char* q = insert_query1.c_str();
+
+//        qstate = mysql_query(conn,q);
+
+        //insert_query1 = "INSERT INTO clients (town) VALUES ('"+ town +"')";
+
+        //if(!qstate)
+        //    {
+        //        cout << endl << "Registration success" << endl;
+        //        system("pause");
+        //        main();
+        //    }
+        //else
+         //   {
+         //       cout << "Query Execution Problem!" << mysql_errno(conn) << endl;
+         //   }
+
+//}
